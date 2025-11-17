1272b120203dd074b6002b5e4948d33960e7dc00
Akheel <ahd.sabag@gmail.com>
2025-11-17 20:59:52 +0200

    implement the first version of the app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+rates = {"USD":1, "EUR":0.92, "ILS":3.7, "GPT":0.8}
+
+@app.route('/health', methods=["GET"])
+def health():
+    return jsonify({"status": "OK"})
+
+def convert_currency(amount, src_currency, dist_currency):
+    return (amount / rates[src_currency]) * rates[dist_currency]
+
+@app.route('/convert', methods=["POST"])
+def convert():
+    data = request.get_json()
+    amount = data.get("amount")
+    src_currency = data.get("from")
+    dist_currency = data.get("to")
+
+    converted = convert_currency(amount, src_currency, dist_currency)
+
+    return jsonify({"converted": converted})
+
+
+    
+if __name__ == "__main__":
+    app.run(port = 8080)
+
